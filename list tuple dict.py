400be4f7318dc11a26430a93a85f3bf3e3ed9595
lelouch0125 <65719296+lelouch0125@users.noreply.github.com>
2020-07-09 00:18:35 +0530

    operations on list , tuples, dictionaries

---
+++ list tuple dict.py
@@ -0,0 +1,31 @@
+#Creating a list and assigning elements to it
+
+"""myList=[1,6,4]
+l2=[8,"panda"]
+l3=["nedim", "rabiya"]
+print(myList)
+myList.append(8)
+print(myList)
+l2.insert(2,10)
+print(l2)
+l3.append("melisa")
+print(l3)"""
+
+#Acessing elements from a tuple
+
+"""tup=(1,8,-4,7)
+print(tup[0])
+print(tup[3])"""
+
+
+#Deleting different dictionary elements
+
+mydict={1:"Nedim", 2:"Rabiya", 3:"Melisa", 4:"Yassine",5:8}
+del mydict[4]
+print(mydict)
+del mydict[5]
+print(mydict)
+mydict.clear()
+print(mydict)
+del mydict
+#the dictionary is deleted along with the memory allocated with it
